e89719c0c0a220a6ee4a68dded3bb1be6bbeca71
katerina-codes <katerinaldg@gmail.com>
2017-03-22 18:37:05 +0000

    Got my tests to pass without modifying them to expect an array. Used .join to turn my array to a string.

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,33 +1,19 @@
 class CaesarCipher
 
-  def convert(string, number) # ("a", 1)
-
+  def convert(string, number)
     new_number = []
-    letters = string.split("") # letters = ["a"]
+    letters = string.split("")
 
-    letters.each do |letter|  # for ["a"]
+    letters.each do |letter|
 
-    code = {0 => "a",
-      1 => "b",
-      2 => "c",
-      3 => "d"}
-      converted_number = code[number] # 1
-      new_number.push(converted_number)
+      code = {0 => "a",
+        1 => "b",
+        2 => "c",
+        3 => "d"}
+        converted_number = code[number]
+        new_number.push(converted_number)
+      end
+      new_number.join
     end
-    new_number
-  end
-
-
-
-  #   if number == 0
-  #     "a"
-  #   elsif number == 1
-  #     "b"
-  #   elsif number == 2
-  #     "c"
-  #   else
-  #     "d"
-  #   end
-  # end
 
 end
