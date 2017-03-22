6ae81530b61061ebd1428743bc19037e6990a4be
katerina-codes <katerinaldg@gmail.com>
2017-03-22 20:47:53 +0000

    Newest test for ('ab', 0) to return 'ab' is green.

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,19 +1,24 @@
 class CaesarCipher
 
-  def convert(string, number)
+  def convert(string, number)               #   "ab", 0
     new_number = []
-    letters = string.split("")
+    letters = string.split("").to_a         #  ["a", "b"]
 
-    letters.each do |letter|
+    if number == 0
+      new_number.push(string)
+    else
+      letters.each do |letter|                #  for "a", "b"
 
-      code = {0 => "a",
-        1 => "b",
-        2 => "c",
-        3 => "d"}
-        converted_number = code[number]
-        new_number.push(converted_number)
+        code = {0 => "a",
+          1 => "b",
+          2 => "c",
+          3 => "d"}
+
+          converted_number = code[number]       # 0 = "a"
+          new_number.push(converted_number)     # ["a"]
+        end
       end
-      new_number.join
-    end
+        new_number.join         # "a", "a"
+      end
 
-end
+    end
--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -7,24 +7,24 @@ describe CaesarCipher do
   expect(encryptor.convert("a", 0)).to eq("a")
   end
 
-  # it "should test that (a, 1) returns (b)" do
-  # encryptor = CaesarCipher.new
-  # expect(encryptor.convert("a", 1)).to eq(["b"])
-  # end
-  #
-  # it "should test that (a, 2) returns (c)" do
-  # encryptor = CaesarCipher.new
-  # expect(encryptor.convert("a", 2 )).to eq(["c"])
-  # end
-  #
-  # it "should test that (a, 3) returns (d)" do
-  # encryptor = CaesarCipher.new
-  # expect(encryptor.convert("a", 3)).to eq("d")
-  # end
+  it "should test that (a, 1) returns (b)" do
+  encryptor = CaesarCipher.new
+  expect(encryptor.convert("a", 1)).to eq("b")
+  end
 
-  # it "should test that ('ab', 0) returns ('ab')" do
-  # encryptor = CaesarCipher.new
-  # expect(encryptor.convert("ab", 0)).to eq("ab")
-  # end
+  it "should test that (a, 2) returns (c)" do
+  encryptor = CaesarCipher.new
+  expect(encryptor.convert("a", 2 )).to eq("c")
+  end
+
+  it "should test that (a, 3) returns (d)" do
+  encryptor = CaesarCipher.new
+  expect(encryptor.convert("a", 3)).to eq("d")
+  end
+
+  it "should test that ('ab', 0) returns ('ab')" do
+  encryptor = CaesarCipher.new
+  expect(encryptor.convert("ab", 0)).to eq("ab")
+  end
 
 end
