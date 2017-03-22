9688bbb2a8677185bbf746ad9eb56d8cdde2bdec
katerina-codes <katerinaldg@gmail.com>
2017-03-22 22:01:15 +0000

    Added text to test that when we increment 'a' by 3 it returns 'd'

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -12,16 +12,16 @@ describe CaesarCipher do
   expect(encryptor.convert("a", 1)).to eq("b")
   end
 
-  it "should test that (a, 2) returns (c)" do
+  it "should test that when we increase 'a' by 2 it returns 'c'" do
   encryptor = CaesarCipher.new
   expect(encryptor.convert("a", 2 )).to eq("c")
   end
-  #
-  # it "should test that (a, 3) returns (d)" do
-  # encryptor = CaesarCipher.new
-  # expect(encryptor.convert("a", 3)).to eq("d")
-  # end
-  #
+
+  it "should test that when we increase 'a' by 3 it returns 'd'" do
+  encryptor = CaesarCipher.new
+  expect(encryptor.convert("a", 3)).to eq("d")
+  end
+
   # it "should test that (ab, 0) returns (ab)" do
   # encryptor = CaesarCipher.new
   # expect(encryptor.convert("ab", 0)).to eq("ab")
