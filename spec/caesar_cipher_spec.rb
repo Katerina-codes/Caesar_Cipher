a2e113ec48c54b91b55f0f6cca205764047933db
katerina-codes <katerinaldg@gmail.com>
2017-03-22 22:06:36 +0000

    Added test to test that when we increment 'ab' by 1 'bc' is returned.

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -27,11 +27,11 @@ describe CaesarCipher do
   expect(encryptor.convert("ab", 0)).to eq("ab")
   end
 
-  # it "should test that (ab, 1) returns (bc)" do
-  # encryptor = CaesarCipher.new
-  # expect(encryptor.convert("ab", 0)).to eq("ab")
-  # end
-  #
+  it "should test that when 'ab' is increased by 1 it returns 'bc'" do
+  encryptor = CaesarCipher.new
+  expect(encryptor.convert("ab", 0)).to eq("ab")
+  end
+
   # it "should test that (cat, 5) returns (hfy)" do
   # encryptor = CaesarCipher.new
   # expect(encryptor.convert("cat", 5)).to eq("hfy")
