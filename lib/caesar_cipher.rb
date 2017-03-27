c88edd2861525e838a6c49cf9941244389de2305
katerina-codes <katerinaldg@gmail.com>
2017-03-27 19:44:05 +0100

    Re-worked code so it was in different methods. Fixed tests to work with my new code.

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -11,84 +11,103 @@ class CaesarCipher
   end
 
   def convert(string, number)
-    new_number = []
-    letters = string.downcase.split("").to_a
+    # string = get_string                                 # hi
+    # number = get_shift_number                           # 1
 
-    if number == 0
-      new_number.push(string)
+    new_number = []                                     # []
+    letters = string.downcase.split("")          #  "h", "i"
+
+    if number == 0                                       #
+      return new_number.push(string).join                            # "hi"
     else
       letters.each do |letter|
-
-        letter_to_number = {"a" => 1,
-          "b" => 2,
-          "c" => 3,
-          "d" => 4,
-          "e" => 5,
-          "f" => 6,
-          "g" => 7,
-          "h" => 8,
-          "i" => 9,
-          "j" => 10,
-          "k" => 11,
-          "l" => 12,
-          "m" => 13,
-          "n" => 14,
-          "o" => 15,
-          "p" => 16,
-          "q" => 17,
-          "r" => 18,
-          "s" => 19,
-          "t" => 20,
-          "u" => 21,
-          "v" => 22,
-          "w" => 23,
-          "x" => 24,
-          "y" => 25,
-          "z" => 26}
-
-          if letter == " " || letter == "!"
-            new_number.push(letter)
-          else
-            converted_number = letter_to_number[letter]
-            updated_converted_number = converted_number.to_i + number.to_i 
-          end
-
-          if updated_converted_number.to_i >= 27
-            updated_converted_number = updated_converted_number - 26
-          end
-
-          number_to_letter = {1 => "a",
-            2 => "b",
-            3 => "c",
-            4 => "d",
-            5 => "e",
-            6 => "f",
-            7 => "g",
-            8 => "h",
-            9 => "i",
-            10 => "j",
-            11 => "k",
-            12 => "l",
-            13 => "m",
-            14 => "n",
-            15 => "o",
-            16 => "p",
-            17 => "q",
-            18 => "r",
-            19 => "s",
-            20 => "t",
-            21 => "u",
-            22 => "v",
-            23 => "w",
-            24 => "x",
-            25 => "y",
-            26 => "z"}
-
-            encrypted_letter = number_to_letter[updated_converted_number]
-            new_number.push(encrypted_letter)
-          end
-        end
-        new_number.join
+        if letter == " " || letter == "!"
+          new_number.push(letter)
+        else                     # "h" -> "i"
+          updated_converted_number = letter_to_number(letter, number)  # 9
+          encrypted_letter = number_to_letter(updated_converted_number) # i
+          new_number.push(encrypted_letter)
+        end                # new_number.push("i")
       end
+    end
+    new_number.join
+  end
+
+
+  def letter_to_number(letter, number)
+    letter_to_number = {"a" => 1,
+      "b" => 2,
+      "c" => 3,
+      "d" => 4,
+      "e" => 5,
+      "f" => 6,
+      "g" => 7,
+      "h" => 8,
+      "i" => 9,
+      "j" => 10,
+      "k" => 11,
+      "l" => 12,
+      "m" => 13,
+      "n" => 14,
+      "o" => 15,
+      "p" => 16,
+      "q" => 17,
+      "r" => 18,
+      "s" => 19,
+      "t" => 20,
+      "u" => 21,
+      "v" => 22,
+      "w" => 23,
+      "x" => 24,
+      "y" => 25,
+      "z" => 26}
+      #
+      # if letter == " " || letter == "!"
+      #   new_number.push(letter)
+
+      converted_number = letter_to_number[letter]
+      updated_converted_number = converted_number.to_i + number.to_i # 8 + 1 = 9
+
+
+      if updated_converted_number.to_i >= 27
+        updated_converted_number = updated_converted_number - 26
+      end
+      updated_converted_number
+    end
+
+    def number_to_letter(updated_converted_number)
+      number_to_letter = {1 => "a",
+        2 => "b",
+        3 => "c",
+        4 => "d",
+        5 => "e",
+        6 => "f",
+        7 => "g",
+        8 => "h",
+        9 => "i",
+        10 => "j",
+        11 => "k",
+        12 => "l",
+        13 => "m",
+        14 => "n",
+        15 => "o",
+        16 => "p",
+        17 => "q",
+        18 => "r",
+        19 => "s",
+        20 => "t",
+        21 => "u",
+        22 => "v",
+        23 => "w",
+        24 => "x",
+        25 => "y",
+        26 => "z"}
+
+        encrypted_letter = number_to_letter[updated_converted_number] # "i"
+        # new_number.push(encrypted_letter)                             # new_number
+      end
+
+      # new_number = CaesarCipher.new.convert   # new_number = convert - new_number = [h, i]
+      # new_number.join        #       "hi"
 
     end
